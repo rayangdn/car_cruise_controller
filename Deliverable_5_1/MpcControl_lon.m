4cc1a793fc88ff6a35cea1259cf9fa3a8f9905af
rayangdn <rayan.gauderon@epfl.ch>
2024-12-15 22:07:01 +0100

    finished part 5 needs to fine tune params

--- Deliverable_5_1/MpcControl_lon.m
+++ Deliverable_5_1/MpcControl_lon.m
@@ -1,93 +1,113 @@
 classdef MpcControl_lon < MpcControlBase
     
     methods
-        function mpc = MpcControl_lon(sys, Ts, H)
-            % Call superclass constructor
-            mpc = mpc@MpcControlBase(sys, Ts, H);
-
-        end
-        
         % Design a YALMIP optimizer object that takes a steady-state state
         % and input (xs, us) and returns a control input
         function ctrl_opti = setup_controller(mpc)
             
-            % Horizon parameters
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   x0           - initial state (estimate)
+            %   V_ref, u_ref - reference state/input
+            %   d_est        - disturbance estimate
+            %   x0other      - initial state of other car
+            % OUTPUTS
+            %   u0           - input to apply to the system
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
             N_segs = ceil(mpc.H/mpc.Ts); % Horizon steps
             N = N_segs + 1;              % Last index in 1-based Matlab indexing
-            
+
             [nx, nu] = size(mpc.B);
             
-            % Targets (not used in relative dynamics)
+            % Targets
             V_ref = sdpvar(1);
             u_ref = sdpvar(1);
-            
-            % Disturbance estimate (not used in tube MPC)
+
+            % Disturbance estimate (Ignore this before Todo 4.1)
             d_est = sdpvar(1);
-            
-            % Initial states (actual and lead car)
+
+            % Initial states
             x0 = sdpvar(nx, 1);
-            x0other = sdpvar(nx, 1);
-            
+            x0other = sdpvar(nx, 1); % (Ignore this before Todo 5.1)
+
             % Input to apply to the system
             u0 = sdpvar(nu, 1);
             
-            % YALMIP Decision variables
-            X = sdpvar(nx, N); % Nominal state trajectory
-            U = sdpvar(nu, N-1); % Nominal input trajectory
-
-
-            % Load pre-computed tube MPC components
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            
+            % NOTE: The matrices mpc.A, mpc.B, mpc.C and mpc.D
+            %       are the DISCRETE-TIME MODEL of your system.
+            %       You can find the linearization steady-state
+            %       in mpc.xs and mpc.us.
+            
+            % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
+          
+            % Load pre-computed tube MPC components for robust control
             data = load('tube_mpc_data.mat');
+            xsafe = [data.xsafe_pos; 0];  % Safe reference point [position; velocity]
             
-            % Safe reference point [position; velocity]
-            xsafe = [data.xsafe_pos; 0];
+            % Cost matrices for nominal trajectory tracking
+            Q_track = data.Q_track;    % State cost matrix
+            R_track = data.R_track;    % Input cost matrix
+
+            % Get tightened constraint sets for robust feasibility
+            % These account for the effect of disturbances
+            Fx_t = data.X_tilde.A;     % Tightened state constraint matrix
+            fx_t = data.X_tilde.b;     % Tightened state constraint bounds
+            Fu_t = data.U_tilde.A;     % Tightened input constraint matrix
+            fu_t = data.U_tilde.b;     % Tightened input constraint bounds
+
+            % Setup optimization variables for nominal trajectories
+            X = sdpvar(nx, N);     % Nominal state trajectory [position; velocity]
+            U = sdpvar(nu, N-1);   % Nominal input trajectory [acceleration]
             
-            % Initialize objective and constraints
-            obj = 0;
-            con = [];
+            % Initialize optimization problem
+            obj = 0;    % Objective function
+            con = [];   % Constraint list
             
-            % Initial state constraint (relative coordinates)
+            % Set initial state constraint in relative coordinates
+            % Transform to deviation from safe reference point
             con = con + (X(:,1) == x0other - x0 - xsafe);
             
-            % Cost matrices from data
-            Q = data.Q_track; % State cost for tracking
-            R = data.R_track; % Input cost for tracking
-            
-            % Compute tightened constraint matrices
-            Fx_t = data.X_tilde.A; % State constraints
-            fx_t = data.X_tilde.b;
-            Fu_t = data.U_tilde.A; % Input constraints
-            fu_t = data.U_tilde.b;
-        
-            
-            % Loop over prediction horizon
+            % Build constraints and objective over prediction horizon
             for k = 1:N-1
-                % Cost function
-                obj = obj + X(:,k)'*Q*X(:,k) + U(:,k)'*R*U(:,k);
+                % System dynamics in relative coordinates
+                % Note: Negative input due to relative dynamics transformation
+                con = con + (X(:,k+1) == mpc.A*X(:,k) - mpc.B*U(:,k));
                 
-                % System dynamics
-                con = con + (X(:,k+1) == mpc.A*X(:,k) - mpc.B*U(:,k)); % '-' becausse of the change of dynamics
-                % State and input constraints
-                con = con + (Fx_t*X(:,k) <= fx_t);  % Tightened state constraints
-                con = con + (Fu_t*U(:,k) <= fu_t);  % Tightened input constraints
+                % Apply tightened state and input constraints
+                con = con + (Fx_t*X(:,k) <= fx_t);    % State constraints
+                con = con + (Fu_t*U(:,k) <= fu_t);    % Input constraints
+                
+                % Stage cost: Penalize deviations from nominal trajectory
+                obj = obj + X(:,k)'*Q_track*X(:,k) + U(:,k)'*R_track*U(:,k);
             end
             
-            % Terminal cost and constraint
-            obj = obj + X(:,N)'*data.Qf*X(:,N);
+            % Add terminal components for stability
+            obj = obj + X(:,N)'*data.Qf*X(:,N);    % Terminal cost
             
-            % Get terminal set constraints
+            % Terminal set constraint for recursive feasibility
             Fterm = data.terminal_set.A;
             fterm = data.terminal_set.b;
             con = con + (Fterm*X(:,N) <= fterm);
             
-            % Compute actual input: u = u_nominal + K*(x - x_nominal)
+            % Compute actual control input
+            % u = u_nominal + K*(x - x_nominal)
             % where x is in relative coordinates
-            delta_x = x0other - x0 - xsafe - X(:,1);
-            con = con + (u0 == U(:,1) + data.K*delta_x);
+            delta_x = x0other - x0 - xsafe - X(:,1);  % State error
+            con = con + (u0 == U(:,1) + data.K*delta_x);  % Tube controller
+            
+            % Store variables for debugging
+            debugVars = {X, U};
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             
             % Return YALMIP optimizer object
             ctrl_opti = optimizer(con, obj, sdpsettings('solver','gurobi'), ...
-                {x0, V_ref, u_ref, d_est, x0other}, {u0, X, U});
+                {x0, V_ref, u_ref, d_est, x0other}, {u0, debugVars{:}});
         end
         
  function [Vs_ref, us_ref] = compute_steady_state_target(mpc, ref, d_est)
@@ -111,22 +131,14 @@ classdef MpcControl_lon < MpcControlBase
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
 
-            % Store the target velocity as our reference
-            % This is the absolute velocity we want to achieve
+            % Set the reference velocity (Vs_ref)
+            % We directly use the requested reference as our target velocity
             Vs_ref = ref;
             
-            % Compute the required steady-state input (throttle) to maintain the reference velocity
-            % At steady state, the state doesn't change, so x(k+1) = x(k)
-            % This means: 0 = A*(Vs_ref - xs) + B*(us_ref - us)
-            % Solving for us_ss:
-            % us_ref = us - A*(Vs_ref-xs)/B
-            % where:
-            %   - A, B are the linearized system matrices for velocity
-            %   - xs is the linearization velocity point
-            %   - us is the linearization input point
-            %   - (Vs_ref-xs) is how far we want to deviate from the linearization point
+            % Set the reference input (us_ref)
+            % The actual control action is determined by the tube MPC controller
+            % This nominal value is not used since the control law provides the input
             us_ref = 0;
-            
 
             % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
